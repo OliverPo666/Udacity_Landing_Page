de4a714a136ab7ff16eb58f04b30548564f66a82
OliverPo666 <55895574+OliverPo666@users.noreply.github.com>
2022-07-03 00:00:54 -0400

    first iteration

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -30,35 +30,51 @@ const navBarList = document.querySelector('#navbar__list');
 //function to create the nav
 function createNavList() {
     for (const section of sections) {
-        console.log(section);
-        const linkName = section.getAttribute('data-nav');
-        const linkId = section.getAttribute('id');
+        // console.log(section);
+        const sectionName = section.getAttribute('data-nav');
         const navLink = document.createElement('li');
-        navLink.innerHTML = `<a class='menu__link' href='#${linkId}'> ${linkName} </a>`
+        navLink.innerHTML = `<a class='menu__link' href='javascript:void(0)'>${sectionName}</a>`;
+        navLink.addEventListener('click', function() {
+            section.scrollIntoView({behavior: "smooth"});
+        });
         navBarList.insertAdjacentElement("beforeend", navLink);
     };
 };
 
 
 //function to check if the section is in vewport
-function checkViewPort(sec){
+function checkViewPort(sec) {
     let sectionView = sec.getBoundingClientRect();
-    return (sectionView.top >= 0 && sectionView.top< window.innerHeight);
+    return (sectionView.top >= -100 && sectionView.bottom < window.innerHeight);
 };
 
-//function to chage class name of section if in view
-function chageClassName(){
-    for(section of sections){
-        if(checkViewPort(section)){
-            if(!section.classList.contains('your-active-class')){
+//function to chage style based on view
+function chageClassName() {
+    for (section of sections) {
+        if (checkViewPort(section)) {
+            //change nva background based on view
+            const linkName = section.getAttribute('data-nav');
+            const links = document.querySelectorAll('.menu__link');
+            for(const link of links){
+                if(linkName === link.innerHTML){
+                    link.style.backgroundColor = 'grey';
+                }
+                else{
+                    link.style.backgroundColor = '#fff'
+                }
+            }
+            //change section background style based on view
+            if (!section.classList.contains('your-active-class')) {
                 section.classList.add('your-active-class');
             }
         }
-        else{
+        else {
             section.classList.remove('your-active-class');
         }
     }
-}
+};
+
+
 
 //create navlist based on the num of sections
 createNavList();
